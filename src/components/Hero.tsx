2cb427744d87f4ec04cf18992aa42e30850d506c
Christopher Mora <morachristopher07@gmail.com>
2025-11-25 20:36:31 -0600

    Initialized portfolio. Started working on Hero Section

---
+++ src/components/Hero.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='mt-16 mx-48 flex flex-col items-start justify-center text-left gap-6 px-4'>
+      <h1 className='font-semibold text-6xl w-130'>Hey. I'm Chris, another developer.</h1>
+      <h3 className='font-semibold text-3xl w-120'>One very passionate about building stuff. Anything.</h3>
+    </div>
+  )
+}
+
+export default Hero;
